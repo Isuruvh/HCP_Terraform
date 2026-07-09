5ce700c65f29169ce5f1b76b59e4d1237a68edec
Isuru Heendeniya <156098319+Isuruvh@users.noreply.github.com>
2026-07-09 14:29:48 +1000

    Add remote state data for network
    Add remote state data block for network configuration.

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+data "terraform_remote_state" "network" {
+  backend = "remote"
+
+  config = {
+    organization = var.tfc_org_name
+    workspaces = {
+          name = var.tfc_network_workspace_name
+    }
+  }
+}
+
 provider "aws" {
   region = "us-west-2"
 }
